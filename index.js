50346f266356fdb6fcf10bc0b0b86e961f589cce
Prasanta Saha <prasanta.work@outlook.com>
2018-02-18 12:47:17 -0800

    adding places auto complete function

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fetch = require("node-fetch");
 const cors = require("cors")();
-
+const googleMaps = require('@google/maps');
+const url = require('url');
 
 const DARK_SKY_END_POINT = "https://api.darksky.net/forecast/";
 const DARK_SKY_API_KEY = "0123456789abcdef9876543210fedcba"; // "d50b3716794d67676d87b72f13a3cb68";
+const GOOGLE_PLACES_API_KEY = "AIzaSyBhLkWlQgsVgvd9XaybJQecqXDSi8fHX4c";
+
+const googleMapsClient = googleMaps.createClient({
+    key: GOOGLE_PLACES_API_KEY
+});
 
 exports.forecast = (req, res) => {
     const requestURL = `${DARK_SKY_END_POINT}${DARK_SKY_API_KEY}${req.url}`;
@@ -20,4 +26,26 @@ exports.forecast = (req, res) => {
                 res.send(data);
             });
     });
+}
+
+exports.placesAutoComplete = (req, res) => {
+    const query = url.parse(req.url, true).query || {};
+    const input = query['input'] || "";
+
+    if (input && input.length > 1 && typeof input === 'string') {
+        googleMapsClient.placesAutoComplete({
+            input: query.input,
+            types: '(cities)'
+        }, function (err, response) {
+            if (!err) {
+                res.json(response.json);
+            } else {
+                res.status(400);
+                res.json(err.json ? err.json : { message: 'Unknown error' });
+            }
+        });
+    } else {
+        res.json({ predictions: [] });
+    }
+
 }

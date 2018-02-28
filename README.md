4eeef4e0f9cf24981d3043b270dbcd87cb76f7f7
Prasanta Saha <prasanta.work@outlook.com>
2018-02-28 10:16:16 -0800

    added support for eslint and updated README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-"# weather-api" 
+### weather-api 
+
+This project uses Google Cloud Function (https://cloud.google.com/functions/) utilizing serverless model.
+
+We need Google Cloud SDK (https://cloud.google.com/sdk/docs/) to deploy the application on Cloud. And we can use Google cloud local emulator (https://cloud.google.com/functions/docs/emulator) run the cloud functions locally.
+
+Use `npm run deploy-function-<function>-<environment>` e.g. `deploy-function-forecasts-local` to deploy the function
+
+`functions list commad will return you the list of functions deployed locally`
+
+```
+┌────────┬───────────┬─────────┬───────────────────────────────────────────────────────────────┐
+│ Status │ Name      │ Trigger │ Resource                                                      │
+├────────┼───────────┼─────────┼───────────────────────────────────────────────────────────────┤
+│ READY  │ forecasts │ HTTP    │ http://localhost:8010/weather-app-micro/us-central1/forecasts │
+├────────┼───────────┼─────────┼───────────────────────────────────────────────────────────────┤
+│ READY  │ maps      │ HTTP    │ http://localhost:8010/weather-app-micro/us-central1/maps      │
+└────────┴───────────┴─────────┴───────────────────────────────────────────────────────────────┘
+```
+To get the list of functions deployed on cloud use the following link: https://console.cloud.google.com/functions/list?project=weather-app-micro
--- index.js
+++ index.js
@@ -1,137 +1,148 @@
-const fetch = require("node-fetch");
-const cors = require("cors")();
+'use strict';
+
+const fetch = require('node-fetch');
+const cors = require('cors')();
 const googleMaps = require('@google/maps');
 const url = require('url');
 const UrlPattern = require('url-pattern');
+const Promise = require('es6-promise').Promise;
 
-const DARK_SKY_END_POINT = "https://api.darksky.net/forecast/";
-const DARK_SKY_API_KEY =  "d50b3716794d67676d87b72f13a3cb68";
+const DARK_SKY_END_POINT = 'https://api.darksky.net/forecast/';
+const DARK_SKY_API_KEY = 'd50b3716794d67676d87b72f13a3cb68';
 
 exports.forecasts = (req, res) => {
-    if (req.method === 'GET') {
-        const requestURL = `${DARK_SKY_END_POINT}${DARK_SKY_API_KEY}${req.url}`;
-        cors(req, res, () => {
-            fetch(requestURL)
-                .then(res => {
-                    return res.json();
-                })
-                .then(data => {
-                    if (data.error && data.code) {
-                        res.status(data.code);
-                    }
-                    res.send(data);
-                });
+  if (req.method === 'GET') {
+    const requestURL = `${DARK_SKY_END_POINT}${DARK_SKY_API_KEY}${req.url}`;
+    cors(req, res, () => {
+      fetch(requestURL)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          if (data.error && data.code) {
+            res.status(data.code);
+          }
+          res.send(data);
         });
-    } else {
-        // Only support GET.
-        res.status(405).end();
-    }
-}
+    });
+  } else {
+    // Only support GET.
+    res.status(405).end();
+  }
+};
 
-
-
-const GOOGLE_PLACES_API_KEY = "AIzaSyBhLkWlQgsVgvd9XaybJQecqXDSi8fHX4c";
+const GOOGLE_PLACES_API_KEY = 'AIzaSyBhLkWlQgsVgvd9XaybJQecqXDSi8fHX4c';
 
 const googleMapsClient = googleMaps.createClient({
-    key: GOOGLE_PLACES_API_KEY
+  key: GOOGLE_PLACES_API_KEY,
 });
 
-getPlacesAutoComplete = query => {
-    const input = query['input'] || "";
-    return new Promise((resolve, reject) => {
-        if (input && input.length > 1 && typeof input === 'string') {
-            googleMapsClient.placesAutoComplete({
-                input: query.input,
-                types: '(cities)'
-            }, (err, response) => {
-                if (!err) {
-                    resolve(response);
-                } else {
-                    reject(err);
-                }
-            });
-        } else {
-            resolve({ predictions: [input] });
+let getPlacesAutoComplete = query => {
+  const input = query['input'] || '';
+  return new Promise((resolve, reject) => {
+    if (input && input.length > 1 && typeof input === 'string') {
+      googleMapsClient.placesAutoComplete(
+        {
+          input: query.input,
+          types: '(cities)',
+        },
+        (err, response) => {
+          if (!err) {
+            resolve(response);
+          } else {
+            reject(err);
+          }
         }
-    });
-}
-
-getReverseGeocode = query => {
-    const latitude = query['latitude'] || "";
-    const longitude = query['longitude'] || "";
-
-    return new Promise((resolve, reject) => {
-        if (latitude && longitude) {
-            googleMapsClient.reverseGeocode({ latlng: `${latitude},${longitude}` }, (err, response) => {
-                if (!err) {
-                    resolve(response);
-                } else {
-                    reject(err);
-                }
-            })
-        } else {
-            reject(new Error("Geocoder.reverseGeocode requires a latitude and longitude."));
-        }
-    });
-}
-
-getPlaceById = query => {
-    const placeid = query['placeid'] || "";
-
-    return new Promise((resolve, reject) => {
-        if(placeid) {
-        googleMapsClient.place({placeid: placeid}, (err, response) => {
-            if (!err) {
-                resolve(response);
-            } else {
-                reject(err);
-            }
-        })
+      );
     } else {
-        reject(new Error("placeid is required."));
+      resolve({ predictions: [input] });
     }
-    });
-}
+  });
+};
+
+let getReverseGeocode = query => {
+  const latitude = query['latitude'] || '';
+  const longitude = query['longitude'] || '';
+
+  return new Promise((resolve, reject) => {
+    if (latitude && longitude) {
+      googleMapsClient.reverseGeocode(
+        { latlng: `${latitude},${longitude}` },
+        (err, response) => {
+          if (!err) {
+            resolve(response);
+          } else {
+            reject(err);
+          }
+        }
+      );
+    } else {
+      reject(
+        new Error('Geocoder.reverseGeocode requires a latitude and longitude.')
+      );
+    }
+  });
+};
+
+let getPlaceById = query => {
+  const placeid = query['placeid'] || '';
+
+  return new Promise((resolve, reject) => {
+    if (placeid) {
+      googleMapsClient.place({ placeid: placeid }, (err, response) => {
+        if (!err) {
+          resolve(response);
+        } else {
+          reject(err);
+        }
+      });
+    } else {
+      reject(new Error('placeid is required.'));
+    }
+  });
+};
 
 exports.maps = (req, res) => {
-    if (req.method === 'GET') {
-        const requestUrl = url.parse(req.url, true);
-        const pattern = new UrlPattern('*(/:path)*');
-        const path = pattern.match(requestUrl.href).path.toLowerCase() || "";
-        const query = requestUrl.query || {};
+  if (req.method === 'GET') {
+    const requestUrl = url.parse(req.url, true);
+    const pattern = new UrlPattern('*(/:path)*');
+    const path = pattern.match(requestUrl.href).path.toLowerCase() || '';
+    const query = requestUrl.query || {};
 
-        cors(req, res, () => {
-            let promise;
-            switch (path) {
-                case "autocomplete":
-                    promise = getPlacesAutoComplete(query);
-                    break;
+    cors(req, res, () => {
+      let promise;
+      switch (path) {
+        case 'autocomplete':
+          promise = getPlacesAutoComplete(query);
+          break;
 
-                case 'reversegeocode':
-                    promise = getReverseGeocode(query);
-                    break;
+        case 'reversegeocode':
+          promise = getReverseGeocode(query);
+          break;
 
-                case 'place':
-                    promise = getPlaceById(query);
-                    break;
+        case 'place':
+          promise = getPlaceById(query);
+          break;
 
-                default:
-                    promise = new Promise((resolve, reject) => {
-                        // path not found.
-                        reject(new Error('No handler configured'));
-                    })
-            }
+        default:
+          promise = new Promise((resolve, reject) => {
+            // path not found.
+            reject(new Error('No handler configured'));
+          });
+      }
 
-            promise.then((response) => {
-                res.json(response.json ? response.json : response);
-            }, ((err) => {
-                res.status(400);
-                res.json({ message: err.message ? err.message : 'Unknown error' });
-            }));
-        });
-    } else {
-        // Only support GET.
-        res.status(404.6).json({ message: 'Verb denied.' });
-    }
-
-}
+      promise.then(
+        response => {
+          res.json(response.json ? response.json : response);
+        },
+        err => {
+          res.status(400);
+          res.json({ message: err.message ? err.message : 'Unknown error' });
+        }
+      );
+    });
+  } else {
+    // Only support GET.
+    res.status(404.6).json({ message: 'Verb denied.' });
+  }
+};
